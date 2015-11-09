1fe4509f1b99b6dacca0c4b69775962fcc8b630e
Yara Grassi Gouffon <vaati2005-git@yahoo.com.br>
2015-11-09 18:52:42 -0500

    DHTML ICE

--- 230/ice/dhtml/script.js
+++ 230/ice/dhtml/script.js
@@ -1,3 +1,7 @@
 function changeColor(color){
 	document.querySelector("#mouseoverTest").style.backgroundColor = color;
 }
+
+function changeAll(property, value){
+	document.getElementById('coolBeans').style[property] = value;
+}
