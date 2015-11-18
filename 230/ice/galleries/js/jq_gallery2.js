00a58eff23d442bd15a4d25bcf216593db942a03
Yara Grassi Gouffon <vaati2005-git@yahoo.com.br>
2015-11-18 14:17:32 -0500

    Gallery ICE stuff

---
+++ 230/ice/galleries/js/jq_gallery2.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var index = 0;
+var captions = ["A fireworks display","A cup of black coffee","A red, red rose","The famous clock"];
+var images = ["images/fireworks.jpg","images/coffee.jpg","images/rose.jpg","images/bigben.jpg"];  
+var clicked = false;
+
+var fireworks = '<img src="images/fireworks.jpg" alt="A fireworks display">';
+var concept = '<img src="images/concept.jpg" alt="A 3d concept">';
+var conceptDesc = '<p>I made this in MS Paint with a broken mouse</p>';
